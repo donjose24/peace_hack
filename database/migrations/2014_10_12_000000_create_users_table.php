c78eb3b04e3135dae798915d73488acd43241aa3
Jose Mari Ramos <ramosjosemari@gmail.com>
2016-09-24 10:35:33 +0800

    Add fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('address');
+            $table->string('facebook_token');
             $table->rememberToken();
             $table->timestamps();
         });
