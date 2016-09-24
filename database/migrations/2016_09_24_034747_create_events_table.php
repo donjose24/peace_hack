f76855c8f4c1de4be9476ea4f488586b8a10a8b0
Jose Mari Ramos <ramosjosemari@gmail.com>
2016-09-24 12:23:26 +0800

    Fix rollback migration errors

--- database/migrations/2016_09_24_034747_create_events_table.php
+++ database/migrations/2016_09_24_034747_create_events_table.php
@@ -17,6 +17,7 @@ class CreateEventsTable extends Migration
             $table->increments('id');
             $table->string('title');
             $table->string('short_description');
+            $table->string('picture');
             $table->string('description');
             $table->string('organizing_body');
             $table->string('organizing_body_email');
--- database/migrations/2016_09_24_041409_create_user_events_table.php
+++ database/migrations/2016_09_24_041409_create_user_events_table.php
@@ -27,6 +27,6 @@ class CreateUserEventsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists();
+        Schema::dropIfExists('users_events');
     }
 }
