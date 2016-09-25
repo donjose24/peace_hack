0a6d47d6f7ba16280ad27b4c7425b192b8740c6e
Jose Mari Ramos <ramosjosemari@gmail.com>
2016-09-25 14:02:02 +0800

    wakokok

--- resources/views/emails/layout.blade.php
+++ resources/views/emails/layout.blade.php
@@ -6,43 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="UTF-8">
         <title>Volteer</title>
-<<<<<<< HEAD
-
-        <style>
-        .btn-eer {
-            background: #d32f2f;
-            color: #fff;
-        }
-
-        .btn-eer:hover {
-            background: #b71c1c;
-            color: #fff;
-        }
-
-        .btn-lg {
-            padding: .75rem 1.5rem;
-            font-size: 1.25rem;
-            border-radius: .3rem;
-        }
-        .btn {
-            display: inline-block;
-            font-weight: 400;
-            line-height: 1.25;
-            text-align: center;
-            white-space: nowrap;
-            vertical-align: middle;
-            cursor: pointer;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-            border: 1px solid transparent;
-            padding: .5rem 1rem;
-            font-size: 1rem;
-            border-radius: .25rem;
-        }
-        </style>
-=======
         <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,700" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="sha384-2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
         <link rel="stylesheet" href="http://volteer.herokuapp.com/css/style.css">
@@ -59,14 +22,11 @@
                 margin-top: .5rem;
             }
         </style>
-        <!-- CSRF Token -->
->>>>>>> d2937c566eaa8da09a4227ab06eb9ee18f90bba3
     </head>
     <body>
         <nav class="navbar navbar-light bg-faded">
             <a class="navbar-brand" href="#">
-<<<<<<< HEAD
-                <img src="https://volteer.herokuapp.com/images/logo-email.png" alt="Volteer">
+            <img src="https://volteer.herokuapp.com/images/logo-email.png" alt="Volteer">
             </a>
             <ul class="nav navbar-nav">
                 <li class="nav-item active" style="list-style: none">
@@ -91,27 +51,14 @@
                     >Go to site</a>
                 </li>
             </ul>
-=======
-                <img src="https://volteer.herokuapp.com/images/logo.png" alt="Volteer">
-            </a>
-            <ul class="nav navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="https://volteer.herokuapp.com/i">Go to site</a>
-                </li>
-            </ul>
-            </form>
->>>>>>> d2937c566eaa8da09a4227ab06eb9ee18f90bba3
         </nav>
 
         @yield('content')
 
-<<<<<<< HEAD
-=======
         <!-- Scripts -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="sha384-VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
         <script src="https://use.fontawesome.com/30b1b5bdff.js"></script>
->>>>>>> d2937c566eaa8da09a4227ab06eb9ee18f90bba3
     </body>
 </html>
