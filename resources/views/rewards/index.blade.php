b486b0cfc98a36d4e26c36f8d486fdfffe032d94
Patrick James G. Lim <patrickjames_lim@yahoo.com>
2016-09-25 10:18:33 +0800

    Added .volt-container to rewards

--- resources/views/rewards/index.blade.php
+++ resources/views/rewards/index.blade.php
@@ -7,8 +7,10 @@
             @foreach($rewards as $reward)
                 <div class="col-lg-4 col-md-12 col-sm-12">
                     <div class="card">
-                        <div class="volt-points"> <i class="fa fa-bolt"></i> {{$reward->cost}}</div>
-                        <img class="card-img-top" src="/images/{{$reward->picture}}" alt="Card image cap">
+                        <div class="volt-container">
+                            <div class="volt-points"> <i class="fa fa-bolt"></i> {{$reward->cost}}</div>
+                            <img class="card-img-top" src="/images/{{$reward->picture}}" alt="Card image cap">
+                        </div>
                         <div class="card-block">
                             <h4 class="card-title dot-ellipsis">{{$reward->title}}</h4>
                             <p class="card-text dot-ellipsis">
