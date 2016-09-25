3c239fdbdb6158458a43a1136fb55b8dd95c6bc5
Patrick James G. Lim <patrickjames_lim@yahoo.com>
2016-09-25 13:30:54 +0800

    Button Email

--- resources/views/emails/layout.blade.php
+++ resources/views/emails/layout.blade.php
@@ -6,6 +6,41 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="UTF-8">
         <title>Volteer</title>
+
+        <style>
+        .btn-eer {
+            background: #d32f2f;
+            color: #fff;
+        }
+
+        .btn-eer:hover {
+            background: #b71c1c;
+            color: #fff;
+        }
+
+        .btn-lg {
+            padding: .75rem 1.5rem;
+            font-size: 1.25rem;
+            border-radius: .3rem;
+        }
+        .btn {
+            display: inline-block;
+            font-weight: 400;
+            line-height: 1.25;
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: middle;
+            cursor: pointer;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            border: 1px solid transparent;
+            padding: .5rem 1rem;
+            font-size: 1rem;
+            border-radius: .25rem;
+        }
+        </style>
     </head>
     <body>
         <nav class="navbar navbar-light bg-faded">
@@ -14,7 +49,7 @@
             </a>
             <ul class="nav navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="https://volteer.herokuapp.com/">Go to site</a>
+                    <a class="nav-link btn btn-eer btn-lg" href="https://volteer.herokuapp.com/">Go to site</a>
                 </li>
             </ul>
         </nav>
