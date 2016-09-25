8609ad11d5256df63088efa25e092a1262666247
Patrick James G. Lim <patrickjames_lim@yahoo.com>
2016-09-25 13:37:52 +0800

    Button Email

--- resources/views/emails/layout.blade.php
+++ resources/views/emails/layout.blade.php
@@ -49,7 +49,26 @@
             </a>
             <ul class="nav navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link btn btn-eer btn-lg" href="https://volteer.herokuapp.com/">Go to site</a>
+                    <a class="nav-link btn btn-eer btn-lg" href="https://volteer.herokuapp.com/"
+                        style=" background: #d32f2f;
+            color: #fff; padding: .75rem 1.5rem;
+            font-size: 1.25rem;
+            border-radius: .3rem; display: inline-block;
+            font-weight: 400;
+            line-height: 1.25;
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: middle;
+            cursor: pointer;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            border: 1px solid transparent;
+            padding: .5rem 1rem;
+            font-size: 1rem;
+            border-radius: .25rem;"
+                    >Go to site</a>
                 </li>
             </ul>
         </nav>
