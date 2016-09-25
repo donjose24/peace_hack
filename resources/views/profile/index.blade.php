3fbc07ed6a7eb05b4cfb3bc41170d8fd1a2282df
Jose Mari Ramos <ramosjosemari@gmail.com>
2016-09-25 12:11:20 +0800

    Fix layout

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -13,16 +13,17 @@
                             </li>
                             <!--<li class="nav-item">
                                 <a class="nav-link" href="#">Past Events</a>
-                            </li>-->
+                                </li>-->
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4 col-md-12 col-sm-12">
-                <div class="card">
-                    @foreach($events as $event)
+            @foreach($events as $event)
+                <div class="col-lg-4 col-md-12 col-sm-12">
+                    <div class="card">
+
                         <div class="volt-points"> <i class="fa fa-bolt"></i> {{$event->points}}  VOLT Points</div>
                         <img class="card-img-top" src="/images/{{$event->picture}}" alt="Card image cap">
                         <div class="card-block ">
@@ -32,13 +33,15 @@
                             <h6 class="date-time"> <i class="fa fa-hourglass-start"></i> {!!date('F d, Y | h:m A', strtotime($event->end_date))!!}</h6>
                             <h6 class="place"><a href="https://goo.gl/maps/aXsQf46ttgG2" target="_blank"><i class="fa fa-map-marker"></i> Tatalon, Quezon City</a></h6>
                             <p class="card-text dot-ellipsis">
-                                {{$event->short_description}}
+                            {{$event->short_description}}
                             </p>
                             <a href="/event/{{$event->id}}" class="btn btn-primary btn-block">View Full Details</a>
                             <a href="#" class="btn btn-outline-danger btn-block" data-toggle="modal" data-target="#myModal">Cancel Attendance</a>
                         </div>
-                    @endforeach
+                    </div>
                 </div>
-            </div>
+
+            @endforeach
         </div> 
+    </div>
 @endsection
