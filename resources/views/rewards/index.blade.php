c75da2cd3f34c7eca262173e3f33f3e4fec73b96
Patrick James G. Lim <patrickjames_lim@yahoo.com>
2016-09-25 12:24:56 +0800

    Added landing style

--- resources/views/rewards/index.blade.php
+++ resources/views/rewards/index.blade.php
@@ -1,37 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid landing">
-        <div class="overlay"></div>
-        <div class="landing-content">
-            <img class="logo-landing" src="img/logo.png" alt="">
-            <h1>Change Lives. Earn Points. Claim Rewards</h1> 
-            <a href="#" class="btn btn-eer btn-lg learn-more">Learn More</a>
-        </div>
-    </div>
-    <div class="container-fluid about">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="eer headline">Peace Comes With Helping People</h1>
-                <br>
-                <h5>With volunteering comes understanding and with understanding comes peace.
-                    Volteer is a gateway for organizations to create their own volunteering programs
-                    and connect them with the people who are willing to help. In exchange for
-                    your effort, you will get <strong>"volts"</strong> which you can use to exchange for rewards.
-                </h5>
-                <br>
-                <a href="#" class="btn btn-eer btn-lg view-programs">View Available Volunteering Programs</a>
-            </div>
-        </div>
-    </div>
-    <div class="container home">
-         <br>
-         <div class="row headline">
-             <h1 class="eer headline">Available Volunteering Programs</h1>
-             <h5>The volunteering programs come from different world organizations.
-                 Start joining and earn your <strong>"volts"</strong> today!
-             </h5>
-         </div>
+    <div class="container rewards">
          <br>
         <div class="row">
             @foreach($rewards as $reward)
